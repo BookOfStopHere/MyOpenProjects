b8afcbd08cb5a9a5c83005b1b53082580df1895e
jockeyyan <jockeyyan@gmail.com>
2013-01-25 23:58:24 +0800

    Commit testing.

---
+++ HelloOpenCV/src/HelloOpenCV.cpp
@@ -0,0 +1,27 @@
+//============================================================================
+// Name        : HelloOpenCV.cpp
+// Author      : Jockey
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+#include <opencv/highgui.h>
+
+int main(int argc, char** argv) {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+    IplImage* pImg = cvLoadImage(argv[1]);
+    cvNamedWindow("Sr", 1);
+    cvShowImage("Sr", pImg);
+    cvWaitKey(0);
+    cvDestroyWindow("Sr");
+    cvReleaseImage(&pImg);
+
+	return 0;
+
+
+}
